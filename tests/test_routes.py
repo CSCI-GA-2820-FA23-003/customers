b590d212131ce2d3ad509790a6c31ac9f5e67231
Andision <48558940+Andision@users.noreply.github.com>
2023-10-16 00:42:32 -0400

    test: add test for create_customer in routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,6 +12,10 @@ from service import app
 from service.models import db
 from service.common import status  # HTTP Status Codes
 
+import json
+from tests.factories import CustomerFactory
+
+
 
 ######################################################################
 #  T E S T   C A S E S
@@ -39,7 +43,106 @@ class TestCustomerServer(TestCase):
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
 
-    def test_index(self):
-        """ It should call the home page """
-        resp = self.client.get("/")
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    def test_create_customer(self):
+        """ Create a new Customer """
+        # Arrange
+
+
+        # customer = CustomerFactory()
+        # logging.debug(customer)
+        # customer.id = None
+        # customer.create()
+        # self.assertIsNotNone(customer.id)
+        # # Fetch it back
+        # found_customer = Customer.find(customer.id)
+        # self.assertEqual(found_customer.id, customer.id)
+        # self.assertEqual(found_customer.first_name, customer.first_name)
+        # self.assertEqual(found_customer.last_name, customer.last_name)
+        # self.assertEqual(found_customer.email, customer.email)
+        # self.assertEqual(found_customer.address, customer.address)
+
+        fake_customer = CustomerFactory()
+        data = json.dumps({
+            "id": fake_customer.id,
+            "first_name": fake_customer.first_name,
+            "last_name": fake_customer.last_name,
+            "email": fake_customer.email,
+            "address": fake_customer.address
+        })
+
+        # Action
+        response = self.client.post(
+            "/customers", 
+            data=data,
+            content_type="application/json"
+        )
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        # Check the data is correct
+        new_json = json.loads(response.data)
+        self.assertEqual(new_json["first_name"], fake_customer.first_name)
+        self.assertEqual(new_json["last_name"], fake_customer.last_name)
+        self.assertEqual(new_json["email"], fake_customer.email)
+        self.assertEqual(new_json["address"], fake_customer.address)
+
+    def test_create_customer_not_json(self):
+        """ Create a new Customer with non-JSON data """
+        # Arrange
+        fake_customer = CustomerFactory()
+
+        # Convert the data to a string instead of JSON
+        data = str({
+            "id": fake_customer.id,
+            "first_name": fake_customer.first_name,
+            "last_name": fake_customer.last_name,
+            "email": fake_customer.email,
+            "address": fake_customer.address
+        })
+
+        # Action
+        response = self.client.post(
+            "/customers", 
+            data=data,
+            content_type="text/plain"  # Send as plain text instead of JSON
+        )
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)  # Expect a 400 error
+
+    def test_create_customer_missing_field(self):
+        """ Create a new Customer with missing fields """
+        # Arrange
+        fake_customer = CustomerFactory()
+        data_dict = {
+            "id": fake_customer.id,
+            "first_name": fake_customer.first_name,
+            "last_name": fake_customer.last_name,
+            "email": fake_customer.email,
+            "address": fake_customer.address
+        }
+
+        required_fields = ["first_name", "last_name", "email", "address"]
+
+        for key in required_fields:
+            bad_data = data_dict.copy()
+                
+
+            # Remove a required field
+            del bad_data[key]
+
+            # Action
+            response = self.client.post(
+                "/customers", 
+                data=json.dumps(bad_data),
+                content_type="application/json"
+            )
+
+            # Assert
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)  # Expect a 400 error
+
+            # Check the error message
+            error_json = json.loads(response.data)
+            self.assertEqual(error_json["error"], "Invalid Customer: missing " + key)
+
